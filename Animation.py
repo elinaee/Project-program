9d64dd241b51682e560dc399ffa5640c25ed8c5c
elinaee <elissa@live.com.pt>
2022-07-14 10:50:38 +0200

    first commit

--- Animation.py
+++ Animation.py
@@ -10,11 +10,13 @@ class Animation:
 
     def update(self):
         self.timer += 1
+
         if self.timer % self.deltaTime == 0:
             if self.index < len(self.images) - 1:
                 self.index += 1
             else:
                 self.index = 0
+                
         self.image = self.images[self.index]
 
     def idle(self):
